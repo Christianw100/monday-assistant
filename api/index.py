3208f68cdb9e7523cc33f1a486f2c18e86e5338c
Christianw100 <Christianisrael@belcorp.biz>
2025-04-24 17:06:28 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,3 @@
-import sys
-import os
-from pathlib import Path
-
-# Agrega el path raíz al sys.path
-current_dir = Path(__file__).resolve().parent
-root_dir = current_dir.parent
-sys.path.append(str(root_dir))
-
-# Importa app correctamente
 from app import app
 
 handler = app
