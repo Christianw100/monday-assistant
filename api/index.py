4dafefe5f6ddda7d8c695682fa0f9f7851977f60
Christianw100 <Christianisrael@belcorp.biz>
2025-04-24 16:56:35 -0500

    Delete api/index.py

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-from pathlib import Path
-
-# Asegura que se puede importar desde el nivel raíz
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-
-from app import app as handler
-
