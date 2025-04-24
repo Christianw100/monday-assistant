b38e521106f7d192c8d6245d79b1ddd78dbb3f19
Christianw100 <Christianisrael@belcorp.biz>
2025-04-24 17:38:49 -0500

    Add files via upload

---
+++ api/index.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({"mensaje": "¡Tu API está corriendo!"})
+
+@app.route("/ping")
+def ping():
+    return jsonify({"status": "ok"})
