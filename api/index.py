28201c2fbf7b6075229348414e5d6f046a9184e7
Christianw100 <Christianisrael@belcorp.biz>
2025-04-24 16:54:08 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,9 @@
+import sys
+import os
+from pathlib import Path
+
+# Asegura que se puede importar desde el nivel raíz
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 from app import app as handler
+
